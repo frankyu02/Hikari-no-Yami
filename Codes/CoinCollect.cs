e72dbcb4a2bd3a658d36e464b01ca54723df2261
frankyu02 <58200308+frankyu02@users.noreply.github.com>
2019-11-26 21:10:35 -0500

    Add files via upload

---
+++ Codes/CoinCollect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Player=gameObject.transform.parent.gameObject; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   private void OnCollisionEnter2D(Collision2D other){
+       if(other.gameObject.tag.Equals("Player")){
+           
+           Destroy(gameObject);
+           
+       }
+   }
+}
