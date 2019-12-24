fbe60ab7cb4350a5bfeedceb8c24449e4688142a
frankyu02 <58200308+frankyu02@users.noreply.github.com>
2019-12-23 19:54:49 -0500

    Add files via upload

---
+++ Player_Stats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Stats : MonoBehaviour
+{
+    public int health=100;
+   public void damage(int damage){
+       health-=damage;
+       if(health>=0){
+           die();
+       }
+   }
+   public void die(){
+       Destroy(gameObject);
+   }
+}
